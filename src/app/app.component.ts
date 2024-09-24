04242f4b001eb629760d92e08c32d01507e302f7
Hi-Dukka <dukka.cua@gmail.com>
2024-09-24 15:43:53 +0530

    Latest-UI-Tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterLinkActive, RouterLink ,RouterOutlet],
+  imports: [RouterLinkActive, RouterLink, RouterOutlet, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -13,3 +14,10 @@ export class AppComponent {
   pageTitle = 'Dashboard';
   title = 'evoke-dashboard';
 }
+
+ const Dashboards = [
+  { title: 'Evolve', description: 'The foundation for seamless experiences', imageUrl: 'assets/evolve.png' },
+  { title: 'Evoke Logo', description: 'Downloadable Logos in various formats', imageUrl: 'assets/evoke-logo.png' },
+  { title: '2024 UX Trends', description: 'Stay ahead with the latest UX trends', imageUrl: 'assets/ux-trends.png' },
+  // Add more box items here
+];
