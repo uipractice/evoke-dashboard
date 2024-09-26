7df3fc683b482069dc13ea77f215706f919a6a2e
Himanshu <hraj@evoketechnologies.com>
2024-09-26 15:02:08 +0530

    Updating FolderStructure and basic Header template as per FIGMA

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,12 @@ import { Routes } from '@angular/router';
 
 /* Routing with Lazy Loading */
 export const routes: Routes = [
-  { path: 'home', loadComponent: () => import('./home/home.component').then(m => m.HomeComponent) },
-  { path: 'dev', loadComponent: () => import('./dev/dev.component').then(m => m.DevComponent) },
-  { path: 'dev/pdf', loadComponent: () => import('./utilities/evoke-style-guide-pdf.component').then(m => m.EvokeStyleGuidePdfComponent) },
-  { path: 'test', loadComponent: () => import('./test/test.component').then(m => m.TestComponent) },
+  { path: 'home', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+  { path: 'dev', loadComponent: () => import('./components/dev/dev.component').then(m => m.DevComponent) },
+  { path: 'dev/pdf', loadComponent: () => import('./shared/evoke-style-guide-pdf.component').then(m => m.EvokeStyleGuidePdfComponent) },
+  { path: 'test', loadComponent: () => import('./components/test/test.component').then(m => m.TestComponent) },
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '**', loadComponent: () => import('./utilities/page-not-found.component').then(m => m.PageNotFoundComponent) }
+  { path: '**', loadComponent: () => import('./shared/page-not-found.component').then(m => m.PageNotFoundComponent) }
 ];
 
 /* Latest Routing via Lazy Loading */
