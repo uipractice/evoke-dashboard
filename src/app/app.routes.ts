614ea9c807b431477ee50b2a76247f0a819f38aa
Himanshu Raj <hraj@evoketechnologies.com>
2024-10-08 12:41:46 +0530

    Dynamic Grid update: Facing some issues in Row2 with Templates combination

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,31 @@
 import { Routes } from '@angular/router';
 
 /* Routing with Lazy Loading */
-export const routes: Routes = [
-  { path: 'home', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
-  { path: 'dev', loadComponent: () => import('./components/dev/dev.component').then(m => m.DevComponent) },
-  { path: 'dev/pdf', loadComponent: () => import('./shared/evoke-style-guide-pdf.component').then(m => m.EvokeStyleGuidePdfComponent) },
-  { path: 'test', loadComponent: () => import('./components/test/test.component').then(m => m.TestComponent) },
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: '**', loadComponent: () => import('./shared/page-not-found.component').then(m => m.PageNotFoundComponent) }
-];
+// export const routes: Routes = [
+//   { path: 'home', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+//   { path: 'dev', loadComponent: () => import('./components/dev/dev.component').then(m => m.DevComponent) },
+//   { path: 'dev/pdf', loadComponent: () => import('./shared/evoke-style-guide-pdf.component').then(m => m.EvokeStyleGuidePdfComponent) },
+//   { path: 'test', loadComponent: () => import('./components/test/test.component').then(m => m.TestComponent) },
+//   { path: '', redirectTo: 'home', pathMatch: 'full' },
+//   { path: '**', loadComponent: () => import('./shared/page-not-found.component').then(m => m.PageNotFoundComponent) }
+// ];
 
 /* Latest Routing via Lazy Loading */
-// export const routes: Routes = [
-//   { path: 'home', loadComponent: () => import('./dashboard/dashboard.component').then(m => m.DashboardComponent) },
-//   { path: '', redirectTo: 'home', pathMatch: 'full' },
-//   { path: '**', loadComponent: () => import('./utilities/page-not-found.component').then(m => m.PageNotFoundComponent) }
-// ]
+export const routes: Routes = [
+  { path: 'Dashboard', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+  { path: 'AI', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+  { path: 'Cloud', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+  { path: 'IT', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+  { path: 'Java', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+  { path: 'LowCode', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+  { path: '.NET', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+  { path: 'OpenSource', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+  { path: 'Oracle', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+  { path: 'Pega', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+  { path: 'QA', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+  { path: 'RPA', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+  { path: 'Salesforce', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+  { path: 'UI', loadComponent: () => import('./components/home/home.component').then(m => m.HomeComponent) },
+  { path: '', redirectTo: 'Dashboard', pathMatch: 'full' },
+  { path: '**', loadComponent: () => import('./shared/page-not-found.component').then(m => m.PageNotFoundComponent) }
+];
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        headerBlue: '#1B154C',
+        headerOrange: '#F16A22',
+        headerWhite: '#FFF',
+      },
+      // gridTemplateColumns: {
+      //   '1.5': 'repeat(2, 1fr) 0.5fr', // Custom grid with 1.5 columns
+      // }
+    },
   },
   plugins: [],
 }
